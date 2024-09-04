c9933bd7cc270fe60339defa5559574a86ebd2df
BansosHunter <seekingforbansos@gmail.com>
2024-09-04 07:28:09 +0700

    Ubah dikit Home

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -148,9 +148,58 @@
     </div>
 </section>
 <!-- Media -->
-<section class="page-section" id="media"></section>
+<section class="page-section" id="media">
+    <div class="container">
+        <div class="text-center">
+            <!-- <h2 class="section-heading text-uppercase">Follow Us</h2>
+            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3> -->
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="Public/assets/img/team/1.jpg" alt="..." />
+                    <h4>Parveen Anand</h4>
+                    <p class="text-muted">Lead Designer</p>
+                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Twitter Profile"><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Facebook Profile"><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="Public/assets/img/team/2.jpg" alt="..." />
+                    <h4>Diana Petersen</h4>
+                    <p class="text-muted">Lead Marketer</p>
+                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Diana Petersen Twitter Profile"><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Diana Petersen Facebook Profile"><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Diana Petersen LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="Public/assets/img/team/3.jpg" alt="..." />
+                    <h4>Larry Parker</h4>
+                    <p class="text-muted">Lead Developer</p>
+                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker Twitter Profile"><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker Facebook Profile"><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-8 mx-auto text-center">
+                <p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+            </div>
+        </div>
+    </div>
+</section>
 <!-- Schedule -->
-<section class="page-section bg-light" id="news"></section>
+<section class="page-section" id="schedule">
+    <header class="masthead">
+        <div class="container">
+        </div>
+    </header>
+</section>
 <!-- About-->
 <section class="page-section" id="about">
     <div class="container">
@@ -222,7 +271,7 @@
     </div>
 </section>
 <!-- Team-->
-<section class="page-section bg-light" id="team">
+<!-- <section class="page-section bg-light" id="team">
     <div class="container">
         <div class="text-center">
             <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
@@ -266,7 +315,7 @@
             </div>
         </div>
     </div>
-</section>
+</section> -->
 <!-- Clients-->
 <!-- <div class="py-5">
     <div class="container">
