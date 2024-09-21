1c5ad8892cda7d21e79b67d4cdf32aff7bda8fe7
edbul69 <ejehezkiel@gmail.com>
2024-09-21 22:51:17 +0700

    UPDATE BUKU

--- app/Views/Home/pelayanan/aba.php
+++ app/Views/Home/pelayanan/aba.php
@@ -6,20 +6,29 @@
 <header class="masthead" id="aba">
 </header>
 
-<section class="page-section bg-black page-pelayanan" id="aba-body">
+<section class="page-section bg-black page-pelayanan pb-0" id="aba-body">
     <div class="container">
         <h2 class="fw-bold text-white text-center mb-3">Ayo Baca Alkitab</h2>
         <div class="d-flex justify-content-center align-items-center mt-3">
             <div class="divider-bar"></div>
         </div>
-        <div class="text-center text-white my-3">
-            <em class="fs-5">“Aku berkata kepadamu: Sesungguhnya barangsiapa mendengar perkataan-Ku dan percaya kepada Dia yang mengutus Aku, ia mempunyai hidup yang kekal dan tidak turut dihukum, sebab ia sudah pindah dari dalam maut ke dalam hidup”</em>
-            <p class="mt-2"><strong>Yohanes 5:24</strong></p>
+    </div>
+</section>
+
+<section class="page-section bg-black p-0 m-0" id="book">
+    <div class="card">
+        <div class="cover">
+            <img src="Public/assets/img/book/cover.jpg" alt="">
+            <!-- <img src="Public/assets/img/book/back.jpg" alt=""> -->
+        </div>
+        <div class="content">
+            <h2>Yohanes 5:24</h2>
+            <p>“Aku berkata kepadamu: Sesungguhnya barangsiapa mendengar perkataan-Ku dan percaya kepada Dia yang mengutus Aku, ia mempunyai hidup yang kekal dan tidak turut dihukum, sebab ia sudah pindah dari dalam maut ke dalam hidup”</p>
         </div>
     </div>
 </section>
 
-<section class="page-section bg-black p-0 m-0">
+<section class="page-selection bg-black">
 
 </section>
 
